f08954e3da3cc7f1ff6d35526616955addaa2c67
rayveng1 <rayvengsell@gmail.com>
2024-09-17 12:05:12 -0500

    Initial Commit: Spring Project Setup

---
+++ src/main/java/com/wos/matchymatchy/MatchyMatchyApplication.java
@@ -0,0 +1,13 @@
+package com.wos.matchymatchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchyMatchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchyMatchyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wos/matchymatchy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wos.matchymatchy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MatchyMatchyApplication.class);
+	}
+
+}
