de326ba407c07d8bc811d4f1faf78c355c049bb3
rayveng1 <rayvengsell@gmail.com>
2024-09-18 20:00:37 -0500

    Add location stuff

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,9 +1,41 @@
-function toggleArrow(element){
+function toggleArrow(element) {
     console.log(element.innerHTML)
     element.innerHTML = (element.innerHTML.trim() === "&lt;") ? "&gt;" : "&lt;"
 }
 
-function showSidebar(){
+const successCallback = (position) => {
+    sendLocationToServer(position.coords.latitude, position.coords.longitude);
+};
 
-    // element.classList.add("details")
+const errorCallback = (error) => {
+    console.log(error);
+};
+
+function getCurrentLocation() {
+    // Check if location has been retrieved already
+    if (!localStorage.getItem("locationFetched")) {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
 }
+
+function sendLocationToServer(lat, lon) {
+    console.log(`lat: ${lat}`);
+    console.log(`lon: ${lon}`);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/savelocation", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Mark that location has been fetched and prevent reload loop
+            localStorage.setItem("locationFetched", "true");
+            window.location.reload();
+        }
+    };
+
+    var data = JSON.stringify({ Latitude: lat, Longitude: lon });
+    xhr.send(data);
+}
+
+// Call this function on page load or based on user action
+getCurrentLocation();
