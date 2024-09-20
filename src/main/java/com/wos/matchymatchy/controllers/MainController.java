509f03f232dfac2ff093960e120390256e100e31
rayveng1 <97568993+rayveng1@users.noreply.github.com>
2024-09-20 17:00:40 -0600

    Revert "Bruh. Trying to resolve conflicts."

--- src/main/java/com/wos/matchymatchy/controllers/MainController.java
+++ src/main/java/com/wos/matchymatchy/controllers/MainController.java
@@ -2,12 +2,17 @@ package com.wos.matchymatchy.controllers;
 
 import com.wos.matchymatchy.models.ApiResponse;
 import com.wos.matchymatchy.models.Location;
+import com.wos.matchymatchy.models.MainPlace;
+import com.wos.matchymatchy.models.Place;
 import com.wos.matchymatchy.services.ApiService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -25,34 +30,31 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String index(HttpSession session, Model model) throws Exception {
+    public String index(@ModelAttribute("mainPlace") MainPlace mainPlace, HttpSession session, Model model) throws Exception {
 
+        if (session.getAttribute("latitude") == null){
+            return "index2.jsp";
+
+        }
         double lat = (double) session.getAttribute("latitude");
         double lng = (double) session.getAttribute("longitude");
 
         Location location = new Location(lat, lng);
 
         ApiResponse response = apiService.getApiResponse(location);
-        model.addAttribute("response", response);
 
-
-
-//     public String index(Model model, HttpSession session) {
-//         ArrayList<Integer> list = new ArrayList<>();
-//         list.add(2);
-//         list.add(4);
-//         list.add(6);
-//         model.addAttribute("categories", list);
-// >>>>>>> main
+        model.addAttribute("places", response.getPlaces());
+        model.addAttribute("categories", getCategorizedPlaces(response.getPlaces()));
+        model.addAttribute("mainPlace", mainPlace);
         return "index2.jsp";
     }
 
     @PostMapping("/savelocation")
     public String location(@RequestBody String request, HttpSession session) {
         JSONObject jsonObject = new JSONObject(request);
-        System.out.println(jsonObject.get("Latitude") == JSONObject.NULL);
+//        System.out.println(jsonObject.get("Latitude") == JSONObject.NULL);
         if (jsonObject.get("Latitude") == JSONObject.NULL) {
-            System.out.println(jsonObject.get("Latitude"));
+//            System.out.println(jsonObject.get("Latitude"));
             session.removeAttribute("latitude");
             session.removeAttribute("longitude");
         } else {
@@ -62,48 +64,49 @@ public class MainController {
         return "index2.jsp";
     }
 
-//    public HashMap<String, List<Place>> getCategorizedPlaces(List<Place> places){
-//        HashMap<String, List<Place>> hm = new HashMap<>();
-//
-//        for (Place place : places){
-//            String category = getMainCategory(place.Types);
-//            if (!hm.containsKey(category)){
-//                hm.put(category, new ArrayList<>().add(place));
-//            } else {
-//                hm.get(category).add(place);
-//            }
-//        }
-//
-//        return hm;
-//    }
-
     public static String getMainCategory(List<String> categories){
         for (String category : categories) {
+            if ((category.contains("car") && !category.contains("care"))) {
+                return "Car";
+            }
             switch (category) {
                 case "department_store":
-                    return "department_store";
+                    return "Department Store";
                 case "food":
-                    return "food";
-                case "car":
-                    return "car";
+                    return "Food";
                 case "finance":
-                    return "finance";
+                    return "Finance";
                 case "insurance":
-                    return "insurance";
+                    return "Insurance";
                 case "entertainment":
-                    return "entertainment";
+                    return "Entertainment";
                 case "travel":
-                    return "travel";
+                    return "Travel";
                 case "hotel_lodging":
-                    return "hotel_lodging";
+                    return "Hotel/Lodging";
                 case "fitness_centers":
-                    return "fitness_centers";
+                    return "Fitness Centers";
                 case "theme_parks":
-                    return "theme_parks";
-                default:
-                    return "unknown_category"; // Optional: Handle any unknown category
+                    return "Theme Parks";// Optional: Handle any unknown category
             }
         }
         return "unknown_category";
+    }
+
+
+    public HashMap<String, List<Place>> getCategorizedPlaces(List<Place> places){
+        HashMap<String, List<Place>> hm = new HashMap<>();
+
+        for (Place place : places){
+            System.out.println("test - "+ place.getEditorialSummary().getText());
+
+            String category = getMainCategory(place.getTypes());
+            if (!hm.containsKey(category)){
+                hm.put(category, new ArrayList<>(){{add(place);}});
+            } else {
+                hm.get(category).add(place);
+            }
+        }
+        return hm;
     }
 }
